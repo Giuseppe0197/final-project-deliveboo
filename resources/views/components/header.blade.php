a53d2caacf96e33270077499c0757aab029deb05
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2022-03-08 17:06:54 +0100

    header fixed

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -9,7 +9,7 @@
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
-                <div class="navbar-nav d-flex align-items-center ">
+                <div class="navbar-nav d-flex align-items-end hamb-menu-clicked">
                     <a class="nav-link" aria-current="page" href="#">Inizia</a>
                     <a class="nav-link" href="#">I nostri prodotti e servizi</a>
                     <a class="nav-link" href="#">Assistenza</a>
