33baf576345c7ecdc54d3db0df19a0ff82628ca6
Andreea1696 <andreeapetrescu16@gmail.com>
2022-03-08 23:51:09 +0100

    Added Jumbotron in Homepage

--- public/js/app.js
+++ public/js/app.js
@@ -41698,7 +41698,7 @@ var render = function () {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "restaurant-search d-flex" }, [
+  return _c("div", { staticClass: "restaurant-search d-flex container" }, [
     _c("div", { staticClass: "search-bar-checkbox container-fluid" }, [
       _c("div", { staticClass: "row" }, [
         _c("input", {
@@ -54588,8 +54588,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Gabri\es finale doc\final-project-deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Gabri\es finale doc\final-project-deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\petre\OneDrive\Documenti\Boolean\Laravel\final-project-deliveboo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\petre\OneDrive\Documenti\Boolean\Laravel\final-project-deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
